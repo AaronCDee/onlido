ca53072fa0d885126b6d041956c0b5b770c46940
AaronCDee <aaroncdee@pop-os.localdomain>
2023-07-01 22:00:41 +0200

    adding all changes to remote

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    validates :name,        presence:      true, 
-                            length:        { maximum: 50 } 
+    validates :name,        presence:      true,
+                            length:        { maximum: 50 }
 
     validates :email,       presence:      true,
-                            uniqueness:    true, 
-                            length:        { maximum: 255 }, 
-                            format:        { with: VALID_EMAIL_REGEX } 
+                            uniqueness:    true,
+                            length:        { maximum: 255 },
+                            format:        { with: VALID_EMAIL_REGEX }
 
     validates :password,    presence:      true, 
-                            length:        { minimum: 6 } 
+                            length:        { minimum: 6 }
 
     private 
         def clean_email
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -47,7 +47,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "name should be present" do
     @user.name = ""
-    assert_not @user.valid? 
+    assert_not @user.valid?
   end
 
 end
